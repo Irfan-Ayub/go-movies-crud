832bf0edfc7527735348381487171c82089ed2b9
Muhammad Irfan Ayub <irfan.ayub@securiti.ai>
2023-10-29 10:28:50 +0500

    Added Movie and Director Struct

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
